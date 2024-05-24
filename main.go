35eb2333ad2b2371c6c16a5c09fbbb49970e5f9e
cxp-13 <lantianlaoli@gmail.com>
2024-05-24 11:55:25 +0800

    add log;

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"ai_bot/handler"
 	"ai_bot/middlerware"
 	wit_ai "ai_bot/wit-ai"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"os"
@@ -17,6 +18,10 @@ func main() {
 	// 初始化 Redis 客户端
 	cache.InitRedis()
 	wit_ai.Init(os.Getenv("WIT_AI_TOKEN"))
+
+	//打印环境变量
+	fmt.Println("WIT_AI_TOKEN:", os.Getenv("WIT_AI_TOKEN"))
+	fmt.Println("REDIS_ADDR:", os.Getenv("REDIS_ADDR"))
 
 	// 注册路由和处理函数
 	r.POST("/storePrivateKey", handler.HandleStorePrivateKey)
